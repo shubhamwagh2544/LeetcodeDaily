117bf0b8a9e06d6dff5d06c03c76876c98da17d7
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-03-07 01:10:35 +0530

    optimal ans

--- march2023/Day6_LC1539.java
+++ march2023/Day6_LC1539.java
@@ -17,7 +17,9 @@ public class Day6_LC1539 {
         //int k = 5;
         //System.out.println(getKthNumberInSorted(array, k));     // brute
         //System.out.println(getKthNumberInSortedAnother(array, k));  // brute
-        System.out.println(getKthNumberInSortedAnotherBrute(array, k));     // brute
+        //System.out.println(getKthNumberInSortedAnotherBrute(array, k));     // brute
+
+        System.out.println(getKthNumberInSortedOptimal(array, k));  // binary-search
     }
     private static int getKthNumberInSorted(int[] array, int k) {
         for (int n : array) {
@@ -63,5 +65,31 @@ public class Day6_LC1539 {
             if (k == 0) return i;
         }
         return -1;
+    }
+    /*
+            arr[i] - (i+1) => denotes how many numbers missed before arr[i]
+            so we are finding mid
+            if
+            arr[mid] - (mid+1) = 0 (no numbers dropped)
+            arr[mid] - (mid+1) < k (numbers dropped are less than k :> means Kth number is to right :> hence start=mid+1
+            else end = mid-1 (kth number is to the left only)
+            hence in any case (start + k) leads to kth number
+
+            eg. suppose numbers dropped are 3 to left and k = 2 :> then kth number is to left
+            eg. suppose number dropped are 1 and k = 3 :> then kth number is to right (shift start to mid+1)
+     */
+    private static int getKthNumberInSortedOptimal(int[] array, int k) {
+        int start = 0;
+        int end = array.length-1;
+        while (start <= end) {
+            int mid = (start+end)/2;
+            if (array[mid]-(mid+1) < k) {
+                start = mid+1;
+            }
+            else {
+                end = mid-1;
+            }
+        }
+        return start+k;
     }
 }
